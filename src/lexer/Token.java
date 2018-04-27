87e8e95d4b326d18f03e0b836236f2e8d488766c
EasonJackson <pyhanson@gmail.com>
2018-04-27 18:40:59 -0400

    Update tags in Tag class

--- src/lexer/Token.java
+++ src/lexer/Token.java
@@ -1,4 +1,14 @@
 package lexer;
 
 public class Token {
+    public final int tag;
+
+    Token(int tag) {
+        this.tag = tag;
+    }
+
+    @Override
+    public String toString() {
+        return "" + (char) tag;
+    }
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,18 +1,51 @@
 /**
  *
+ * A simply and easy use compiler front end
+ * @Author Eason
+ * @Time April 2018
+ *
+ * / Language grammar /
+ * // Program //
  * program -> block
  * block -> { decls stmts }
  * decls -> decls decl | es
  * decl -> type id ;
  * type -> type [ nUID ] | basic
  * stmts -> stmts stmt | es
+ *
+ * // Statement //
+ * stmt -> loc = bool ;
+ *         | if ( bool ) stmt
+ *         | if ( bool ) stmt else stmt
+ *         | while ( bool ) stmt
+ *         | do stmt while ( bool )
+ *         | break ;
+ *         | block
+ * loc -> loc [ boo I ] | id
+ *
+ * // Expression //
+ * bool -> bool || join | join
+ * join -> join && equality | equality
+ * equality -> equality == rel | equality ! = rel | rel
+ * rel -> expr < expr | expr <= expr | expr >= expr
+ * expr -> expr | expr
+ * expr -> expr + term | expr - term | term
+ * term -> term * unary | term / unary | unary
+ * unary -> ! unary | - unary | factor
+ * factor -> ( bool ) | loc | num | real | true | false
  */
 package main;
 
 
+import lexer.Lexer;
+import parser.Parser;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        Lexer lexer = new Lexer();
+        Parser parser = new Parser(lexer);
+        parser.program();
+        System.out.write('\n');
     }
 }
