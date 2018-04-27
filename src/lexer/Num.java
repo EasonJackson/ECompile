d7fe6500f8eeb2e6213d9d935dc99bdb9fc5e7db
EasonJackson <pyhanson@gmail.com>
2018-04-27 18:59:41 -0400

    Update Word class

--- src/lexer/Num.java
+++ src/lexer/Num.java
@@ -1,4 +1,15 @@
 package lexer;
 
 public class Num extends Token {
+    public final int value;
+
+    Num(int value) {
+        super(Tag.NUM);
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "" + value;
+    }
 }
