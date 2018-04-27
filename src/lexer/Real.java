3666de2054f63dbd389e6b8752b60b566bc92c04
EasonJackson <pyhanson@gmail.com>
2018-04-27 19:01:44 -0400

    Update Real class

--- src/lexer/Real.java
+++ src/lexer/Real.java
@@ -1,4 +1,15 @@
 package lexer;
 
 public class Real extends Token {
+    public final float value;
+
+    Real(float value) {
+        super(Tag.REAL);
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "" + value;
+    }
 }
