baaf65f116afdfb402d6f2c69869461874bba3fe
EasonJackson <pyhanson@gmail.com>
2018-04-28 17:36:41 -0400

    Complete scan method in lexer

--- src/lexer/Lexer.java
+++ src/lexer/Lexer.java
@@ -24,14 +24,111 @@ public class Lexer {
     }
 
     void readch() throws IOException {
-
+        peek = (char) System.in.read();
     }
 
     boolean readch(char c) throws IOException {
-
+        readch();
+        if (peek != c) {
+            return false;
+        }
+        peek = ' ';
+        return true;
     }
 
     public Token scan() throws IOException {
+        for (; ; readch()) {
+            if (peek == ' ' || peek == '\t') {
+                continue;
+            } else if (peek == '\n') {
+                line += 1;
+            } else {
+                break;
+            }
+        }
 
+        switch (peek) {
+            case '&':
+                if (readch('&')) {
+                    return Word.and;
+                } else {
+                    return new Token('&');
+                }
+            case '|':
+                if (readch('|')) {
+                    return Word.or;
+                } else {
+                    return new Token('|');
+                }
+            case '=':
+                if (readch('=')) {
+                    return Word.eq;
+                } else {
+                    return new Token('=');
+                }
+            case '!':
+                if (readch('=')) {
+                    return Word.ne;
+                } else {
+                    return new Token('!');
+                }
+            case '<':
+                if (readch('=')) {
+                    return Word.le;
+                } else {
+                    return new Token('<');
+                }
+            case '>':
+                if (readch('=')) {
+                    return Word.ge;
+                } else {
+                    return new Token('>');
+                }
+        }
+
+        if (Character.isDigit(peek)) {
+            int v = 0;
+            do {
+                v = 10 * v + Character.digit(peek, 10);
+                readch();
+            } while (Character.isDigit(peek));
+
+            if (peek != '.') {
+                return new Num(v);
+            }
+
+            float x = v;
+            float d = 10F;
+
+            for ( ; ; ) {
+                readch();
+                if (!Character.isDigit(peek)) {
+                    break;
+                }
+                x = x + Character.digit(peek, 10) / d;
+                d *= 10;
+            }
+            return new Real(x);
+        }
+
+        if (Character.isLetter(peek)) {
+            StringBuffer sb = new StringBuffer();
+            do {
+                sb.append(peek);
+                readch();
+            } while (Character.isLetterOrDigit(peek));
+            String s = sb.toString();
+            Word w = words.get(s);
+            if (w != null) {
+                return w;
+            }
+            w = new Word(s, Tag.ID);
+            words.put(s, w);
+            return w;
+        }
+
+        Token token = new Token(peek);
+        peek = ' ';
+        return token;
     }
 }
