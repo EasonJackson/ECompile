1de474c89a23fdb78181f9f35ce01ffeca46bfe7
EasonJackson <pyhanson@gmail.com>
2018-05-02 11:30:16 -0400

    Id and Op classes

--- src/inter/Id.java
+++ src/inter/Id.java
@@ -1,4 +1,13 @@
 package inter;
 
+import lexer.Word;
+import symbols.Type;
+
 public class Id extends Expr {
+    public int offset;
+
+    public Id(Word word, Type type, int offset) {
+        super(word, type);
+        this.offset = offset;
+    }
 }
--- src/inter/Op.java
+++ src/inter/Op.java
@@ -1,4 +1,17 @@
 package inter;
 
+import lexer.Token;
+import symbols.Type;
+
 public class Op extends Expr {
+    public Op(Token token, Type type) {
+        super(token, type);
+    }
+
+    public Expr reduce() {
+        Expr x = gen();
+        Temp tmp = new Temp(this.type);
+        emit(tmp.toString() + " = " + x.toString());
+        return tmp;
+    }
 }
