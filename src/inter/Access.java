a928aec4eac822b22b81584c5d76c4b75ebdf30c
EasonJackson <pyhanson@gmail.com>
2018-05-06 12:26:02 -0400

    Access class

--- src/inter/Access.java
+++ src/inter/Access.java
@@ -1,4 +1,30 @@
 package inter;
 
+import lexer.Tag;
+import lexer.Word;
+import symbols.Type;
+
 public class Access extends Op {
+    public Id array;
+    public Expr index;
+
+    public Access(Id array, Expr index, Type type) {
+        super(new Word("[]", Tag.INDEX), type);
+        this.array = array;
+        this.index = index;
+    }
+
+    @Override
+    public Expr gen() {
+        return new Access(this.array, index.reduce(), this.type);
+    }
+
+    public void jumping(int t, int f) {
+        emitjumps(reduce().toString(), t, f);
+    }
+
+    @Override
+    public String toString() {
+        return this.array.toString() + "[" + this.index.toString() + "]";
+    }
 }
