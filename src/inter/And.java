8ce75478b970a93d5040f65ef8a6cf4daccd2f5e
EasonJackson <pyhanson@gmail.com>
2018-05-06 11:39:52 -0400

    And

--- src/inter/And.java
+++ src/inter/And.java
@@ -1,4 +1,18 @@
 package inter;
 
+import lexer.Token;
+
 public class And extends Logical {
+    public And(Token token, Expr expr1, Expr expr2) {
+        super(token, expr1, expr2);
+    }
+
+    public void jumping(int t, int f) {
+        int label = f != 0 ? f : newlabel();
+        this.expr1.jumping(0, label);
+        this.expr2.jumping(t, f);
+        if (f == 0) {
+            emitlabel(label);
+        }
+    }
 }
