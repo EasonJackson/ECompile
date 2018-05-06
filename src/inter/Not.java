ef691e529a21ea327e02bb88760de6274cbad619
EasonJackson <pyhanson@gmail.com>
2018-05-06 12:08:52 -0400

    Not class

--- src/inter/Not.java
+++ src/inter/Not.java
@@ -1,4 +1,18 @@
 package inter;
 
+import lexer.Token;
+
 public class Not extends Logical {
+    public Not(Token token, Expr expr) {
+        super(token, expr, expr);
+    }
+
+    public void jumping(int t, int f) {
+        this.expr2.jumping(f, t);
+    }
+
+    @Override
+    public String toString() {
+        return this.op.toString() + " " + this.expr2.toString();
+    }
 }
