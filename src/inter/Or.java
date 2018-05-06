02164b6d96f94163121810f5ff71cfb102588b54
EasonJackson <pyhanson@gmail.com>
2018-05-06 11:38:34 -0400

    Or

--- src/inter/Or.java
+++ src/inter/Or.java
@@ -1,4 +1,18 @@
 package inter;
 
+import lexer.Token;
+
 public class Or extends Logical {
+    public Or(Token token, Expr expr1, Expr expr2) {
+        super(token, expr1, expr2);
+    }
+
+    public void jumping(int t, int f) {
+        int label = t != 0 ? t : newlabel();
+        this.expr1.jumping(label, 0);
+        this.expr2.jumping(t, f);
+        if (t == 0) {
+            emitlabel(label);
+        }
+    }
 }
