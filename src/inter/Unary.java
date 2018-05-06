6b1c57c1235c9d2e2c6ce8f85f4dffd920c2762d
EasonJackson <pyhanson@gmail.com>
2018-05-06 11:03:22 -0400

    Unary class

--- src/inter/Unary.java
+++ src/inter/Unary.java
@@ -1,4 +1,27 @@
 package inter;
 
+import lexer.Token;
+import symbols.Type;
+
 public class Unary extends Op {
+    public Expr expr;
+
+    public Unary(Token token, Expr expr) {
+        super(token, null);
+        this.expr = expr;
+        this.type = Type.max(Type.Int, expr.type);
+        if (this.type == null) {
+            error("Type error");
+        }
+    }
+
+    @Override
+    public Expr gen() {
+        return new Unary(this.op, expr.reduce());
+    }
+
+    @Override
+    public String toString() {
+        return this.op.toString() + " " + this.expr.toString();
+    }
 }
