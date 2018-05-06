020b71128f4b3cbeb1b8f1732950e7442dfc5c66
EasonJackson <pyhanson@gmail.com>
2018-05-06 12:31:59 -0400

    If

--- src/inter/If.java
+++ src/inter/If.java
@@ -1,4 +1,24 @@
 package inter;
 
+import symbols.Type;
+
 public class If extends Stmt {
+    Expr expr;
+    Stmt stmt;
+
+    public If(Stmt stmt, Expr expr) {
+        this.expr = expr;
+        this.stmt = stmt;
+
+        if (this.expr.type != Type.Bool) {
+            this.expr.error("boolean required in if");
+        }
+    }
+
+    public void gen(int b, int a) {
+        int label = newlabel();
+        this.expr.jumping(0, a);
+        emitlabel(label);
+        stmt.gen(label, a);
+    }
 }
--- src/inter/Stmt.java
+++ src/inter/Stmt.java
@@ -1,4 +1,12 @@
 package inter;
 
 public class Stmt extends Node {
+    int after = 0;
+    public static Stmt Null = new Stmt();
+
+    public void gen(int b, int a) {
+
+    }
+
+    public static Stmt Enclosing = Stmt.Null;
 }
