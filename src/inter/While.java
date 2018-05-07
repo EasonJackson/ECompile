19d1984c172b38f99da8374ffd34081cbaf1b61e
EasonJackson <pyhanson@gmail.com>
2018-05-07 19:48:09 -0400

    While

--- src/inter/While.java
+++ src/inter/While.java
@@ -1,4 +1,30 @@
 package inter;
 
+import symbols.Type;
+
 public class While extends Stmt {
+    Expr expr;
+    Stmt stmt;
+
+    public While() {
+        this.expr = null;
+        this.stmt = null;
+    }
+
+    public void init(Expr expr, Stmt stmt) {
+        this.expr = expr;
+        this.stmt = stmt;
+        if (this.expr.type != Type.Bool) {
+            this.expr.error("boolean required in while");
+        }
+    }
+
+    public void get(int b, int a) {
+        this.after = a;
+        this.expr.jumping(0, a);
+        int label = newlabel();
+        emitlabel(label);
+        this.stmt.gen(label, b);
+        emit("goto L" + b);
+    }
 }
