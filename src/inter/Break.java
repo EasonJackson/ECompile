ebecb5c5ff9dd7364e1a099dbaa1d423110a083b
EasonJackson <pyhanson@gmail.com>
2018-05-07 20:46:09 -0400

    Break class

--- src/inter/Break.java
+++ src/inter/Break.java
@@ -1,4 +1,16 @@
 package inter;
 
 public class Break extends Stmt {
+    Stmt stmt;
+
+    public Break() {
+        if (Stmt.Enclosing == null) {
+            error("unenclosed break");
+            this.stmt = Stmt.Enclosing;
+        }
+    }
+
+    public void gen(int b, int a) {
+        emit("goto L" + this.stmt.after);
+    }
 }
